e934144df2b288fb9702f2ff9ef73870a73c6d66
Yazeed123 <almuyaze@my.dom.edu>
2018-02-11 21:49:19 -0600

    Add files via upload
    Thank You page

---
+++ ThankYou.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- <meta  content="tex/html"charset="UTF-8">-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" 
+        integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <!-- FontAwesome JS links -->
+        <!-- End FontAwesome JS links -->
+        <link rel="stylesheet" type="text/css" href "http://fonts.googleapis.com/css?family=Droid Serif">
+        <link rel="stylesheet" type="text/css" href = "Styles/Styles.css"/>
+        
+        <Style>
+		.container{
+			background-color:#FFF;
+			}
+		.btn-green{
+			background-color:#137513;
+			color: #FFFFFF;
+			}
+		
+		</style>
+    </head>
+    <body>
+                    <div class ="container w-50">  
+                    <form action="#" method="post">
+                    <div class="col-lg-12 col-xs-12" id="divLogoHeader">  
+                    <img class="img-fluid" src="images/logo-church-header.png" alt="Saint Juliana Parish" id="imgLogoHeader">
+                        </div ><br><br>
+                        <div class="col-lg-12 col-xs-12">  
+                    <h1> <div class="text-center"> <h1 style="font-family:verdana;"> <strong> Thank you</h1>
+                             <h2>And sorry for your lost </br> </br> We just want you to know </br> </br> 
+                             Saint Juliana Parish are with you.</h2></br> </br>
+                                               </br> </br>
+                        </div >
+                      <div >
+                           <input type="submit" class="btn btn-md btn-block btn-green "  value="Exit">
+                      </div>
+                         <br><br>
+                        </form>
+                      </div>       
+       <!--END container -->
+    </body>
+</html>
