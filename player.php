962753afb7734a6d40a371cc6318b943968e9a45
Luis Gomez <33851141+gomeluis@users.noreply.github.com>
2018-02-21 09:59:37 -0600

    Corrected the description for player.php

--- player.php
+++ player.php
@@ -3,7 +3,7 @@
 
 <head>
 	<!--Author: Luis Gomez 
-    	Description: This allows the user to preview songs that have selected
+    	Description: This allows the user to preview songs they have selected from MusicSelection.php
      -->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
