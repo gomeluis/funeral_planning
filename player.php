7c56251e2a08cbd634312b1dfa8bf748163fef64
Luis Gomez <33851141+gomeluis@users.noreply.github.com>
2018-02-21 09:55:38 -0600

    Added comments

--- player.php
+++ player.php
@@ -2,6 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
+	<!--Author: Luis Gomez 
+    	Description: This allows the user to preview songs that have selected
+     -->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 
